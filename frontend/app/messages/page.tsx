ce5dde5645f662fdfb947a029a8af75f7ac18d63
canalside12 <canalsiderealty@gmail.com>
2025-10-15 22:58:20 -0400

    Create page.tsx

---
+++ frontend/app/messages/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export default function Messages() {
+  return (
+    <div className="flex min-h-screen bg-slate-gray text-graphite p-6">
+      <h1 className="text-2xl heading">Messages Page</h1>
+    </div>
+  );
+}
