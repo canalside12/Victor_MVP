a8df7d27638e184b4ca90397292b36d10519d293
canalside12 <canalsiderealty@gmail.com>
2025-10-15 22:52:25 -0400

    Create page.tsx

---
+++ frontend/app/projects/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+export default function Projects() {
+  return (
+    <div className="flex min-h-screen bg-slate-gray text-graphite p-6">
+      <h1 className="text-2xl heading">Projects Page</h1>
+    </div>
+  );
+}
